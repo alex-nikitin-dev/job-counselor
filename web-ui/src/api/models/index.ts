4e204d4f0ace10fa57fb0db2a01ea8881f8ea99f
Alex Nikitin <alex.nikitin.gm@gmail.com>
2025-06-05 05:11:55 -0400

    Add OpenAPI contract and generate TypeScript client

---
+++ web-ui/src/api/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ApiV1ProfilesIdPutRequest';
+export * from './ApiV1ProfilesPostRequest';
+export * from './EducationItem';
+export * from './ExperienceItem';
+export * from './Language';
+export * from './Profile';
+export * from './Skill';
